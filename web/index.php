64c2ae6a4bd7b577c5959cd95e17d1f786b0b753
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-16 14:36:21 -0500

    adding css

--- web/index.php
+++ web/index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="./css/main.css">
+  <title>CSE 341 Cristhians Ruiz</title>
 </head>
 
 <body>
@@ -12,11 +13,29 @@
     <header>
       <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
     </header>
-    <h1>Homepage</h1>
+    <main>
+      <div class="favoriteQuote">
+        <blockquote>
+          "Do not pray for an easy life; pray for the strength to endure a difficult one"
+          <p>-Bruce Lee-</p>
+        </blockquote>
+      </div>
 
-    <footer>
-      <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
-    </footer>
+
+
+      <section class="extra">
+        <h2 id="major">Bachelor of Science, Applied Technology</h2>
+        <p>The <strong>bachelor’s degree in applied technology</strong> helps students prepare for careers in software and web development,
+          information technology, and other technology-related fields. Students customize their technology education and
+          gain specific technical skills by selecting from a variety of options, including computer support, programming,
+          web and database development, system administration, auto services technology, and computer-aided drafting. This
+          degree is developed and granted by BYU-Idaho.</p>
+
+        <a href="https://www.byupathway.org/degrees"><img src="./assets/laptops.jpeg" alt="applied technology programming image"></a>
+      </section>
+      <footer>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
+      </footer>
   </div>
 </body>
 
