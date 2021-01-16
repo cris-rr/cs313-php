a531a3502589f95ee269208b0b3526834c76e94f
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-16 16:53:13 -0500

    fixing javascript effect on motto

--- web/js/main.js
+++ web/js/main.js
@@ -1,11 +1,12 @@
+let i = 0;
+const speed = 50;
+const quote = '"Do not pray for an easy life; pray for the strength to endure a difficult one"';
+
 function writeMotto() {
-  let i = 0;
-  const speed = 50;
-  const quote = '"Do not pray for an easy life; pray for the strength to endure a difficult one"';
   const motto = document.getElementById("motto");
   if (i < quote.length) {
     motto.innerHTML += quote.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(writeMotto, speed);
   }
 }
