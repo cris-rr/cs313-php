d77e05608b15d90631474fdf92ba459390aacb64
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-24 01:03:14 -0500

    styles

--- web/week03/shoppingcart/index.php
+++ web/week03/shoppingcart/index.php
@@ -41,6 +41,7 @@ function addToCart()
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/main.css">
   <title>Items to shop</title>
 </head>
 
