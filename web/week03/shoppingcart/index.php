e9c5fe7c5f65864c14f4ad9964647b5f85211e64
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-23 21:12:00 -0500

    fixing gallery

--- web/week03/shoppingcart/index.php
+++ web/week03/shoppingcart/index.php
@@ -1,33 +1,42 @@
 <?php
-  session_start();
-  if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = array();
-  }
-  if (!isset($_SESSION['items'])) {
-    $items = array(
-      1 => array(
-        "description" => "Camaro, modern an fast card in black",
-        "price" => 20000,
-        "imgurl" => "../images/camaro.jpg"
-      ),
-      2 => array(
-        "description" => "Hummer, every fits in the hummer, to travel every where",
-        "price" => 35000,
-        "imgurl" => "../images/hummer.jpg"
-      )
-    );
-    $_SESSION['items'] = $items;
-  }
+session_start();
+session_destroy();
+session_start();
 
-  function addToCart(){
-    $item = array(
-      "description" => $_POST[''],
-      "price" => $_POST[''],
-      "imgurl" => $_POST['']
-    );
-    array_push($_SESSION['cart'],$item)
-  }
-  
+if (!isset($_SESSION['cart'])) {
+  $_SESSION['cart'] = array();
+}
+if (!isset($_SESSION['items'])) {
+  $items = array(
+    1 => array(
+      "description" => "Camaro, modern an fast card in black",
+      "price" => 20000,
+      "imgurl" => "../images/camaro.jpg"
+    ),
+    2 => array(
+      "description" => "Hummer, every fits in the hummer, to travel every where",
+      "price" => 35000,
+      "imgurl" => "../images/hummer.jpg"
+    )
+  );
+  $_SESSION['items'] = $items;
+
+  //echo 'items array:';
+  //print_r($_SESSION['items']);
+  // $arr = get_defined_vars();
+  // print_r($arr);
+}
+
+function addToCart()
+{
+  $item = array(
+    "description" => $_POST[''],
+    "price" => $_POST[''],
+    "imgurl" => $_POST['']
+  );
+  array_push($_SESSION['cart'], $item);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,22 +56,20 @@
     <div class="item">
 
       <?php
-      foreach ($$_SESSION['items'] as $item) {
+      foreach ($_SESSION['items'] as $item) {
         $displayItems = "<img class='item-img' src='$item[imgurl]' alt=''>";
-        $displayItems .= "<p class='item-desc'>$items[description]</p>";
-        $displayItems .= "<p class='item-price'>$<span class='price'>$items[price]</span></p>";
-        $displayItems.= "<form method='POST' action='addToCart'>";
+        $displayItems .= "<p class='item-desc'>$item[description]</p>";
+        $displayItems .= "<p class='item-price'>$<span class='price'>$item[price]</span></p>";
+        $displayItems .= "<form method='POST' action='addToCart'>";
         $displayItems .= "<input type='hidden' value='$item[imgurl]' name='imgUrl'>";
-        $displayItems .= "<input type='hidden' value='$items[description]' name='description'>";
-        $displayItems .= "<input type='hidden' value= '$items[price]' name='price'>";
+        $displayItems .= "<input type='hidden' value='$item[description]' name='description'>";
+        $displayItems .= "<input type='hidden' value= '$item[price]' name='price'>";
         $displayItems .= "<input type='submit' name='submit'>";
         $displayItems .= "</form>";
       }
-
+      echo $displayItems;
       ?>
 
-      <?php
-      ?>
     </div>
   </section>
 </main>
