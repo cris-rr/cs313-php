8fbc940447265d3a3f927a7cd41f999a166435ee
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-25 07:01:48 -0500

    fix footer

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,4 +1,4 @@
-<p>&copy; 2020 | Cristhians Ruiz | Lima, Perú |
+<p>&copy; 2021 | Cristhians Ruiz | Lima, Perú |
   <span>
     <a href="https://www.byui.edu/online">BYUI Online learning</a>
   </span>
