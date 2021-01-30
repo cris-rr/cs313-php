b2ae5fe548fe57af7550a9157eb582850f1246ac
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-01-30 00:00:16 -0500

    myDb.sql file

---
+++ db/myDb.sql
@@ -0,0 +1,37 @@
+-- Create database
+-- CREATE DATABASE buddyloan;
+CREATE SCHEMA buddyloan;
+
+-- Users Table
+CREATE TABLE buddyloan.users (
+  userid SERIAL NOT NULL PRIMARY KEY,
+  firstname VARCHAR(100) NOT NULL,
+  lastname VARCHAR(100) NOT NULL,
+  pin SMALLINT NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
+
+-- Buddies Table
+CREATE TABLE buddyloan.buddies (
+  id SERIAL NOT NULL PRIMARY KEY,
+  userid INT NOT NULL REFERENCES buddyloan.users(userid),
+  buddyid INT NOT NULL REFERENCES buddyloan.users(userid),
+  date DATE NOT NULL DEFAULT CURRENT_DATE,
+  balance NUMERIC(10,2)
+);
+
+--Transactions Table
+CREATE TABLE buddyloan.transactions (
+  transactionid SERIAL NOT NULL PRIMARY KEY,
+  description VARCHAR(255) NOT NULL,
+  date DATE NOT NULL DEFAULT CURRENT_DATE,
+  amount NUMERIC(10,2),
+  image_path VARCHAR(255),
+  userid INT NOT NULL REFERENCES buddyloan.users(userid),
+  buddyid INT NOT NULL REFERENCES buddyloan.users(userid)
+);
+
+-- check tables and constraints
+-- \d+ buddyloan.
