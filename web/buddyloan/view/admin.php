dd478f5696d36808e622082b696bc35b1d550224
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-02-08 16:23:22 -0500

    adding form to admin page

--- web/buddyloan/view/admin.php
+++ web/buddyloan/view/admin.php
@@ -19,20 +19,37 @@
   <h1 class="title">Configure your personal information</h1>
   <p>Here you can view and edit your data and credentials</p>
 
-  <main class="data">
-    <section class="info">
-      <h2>Personal details</h2>
-      <p><span class="info-title">First Name: </span>Pedro</p>
-      <p><span class="info-title">Last Name: </span>Perez</p>
-      <p><span class="info-title">Phome: </span>9989999555</p>
-      <p><span class="info-title">Email: </span>pperez@gmail.com</p>
-      <p><span class="info-title">Pin: </span>8734</p>
-    </section>
+  <main class="user-data">
+    <h2>Change your data</h2>
+    <form class="form-basic" id="admin" method="POST" action="">
+      <label for="firstname">First Name:</label>
+      <input class="data" type="text" name="firstname" id="firstname" <?php if (isset($user['firstname'])) {
+                                                                        echo "value='$user[firstname]'";
+                                                                      } ?> required>
+      <label for="lastname">Last Name:</label>
+      <input class="data" type="text" name="lastname" id="lastname" <?php if (isset($user['firstname'])) {
+                                                                      echo "value='$user[firstname]'";
+                                                                    } ?> required>
+      <label for="phone">Phone Number:</label>
+      <input class="data" type="text" name="phone" id="phone" <?php if (isset($user['phone'])) {
+                                                                echo "value='$user[phone]'";
+                                                              } ?> required>
+      <label for="email">Email:</label>
+      <input class="data" type="email" name="email" id="email" <?php if (isset($user['email'])) {
+                                                                  echo "value='$user[email]'";
+                                                                } ?> required>
+      <label for="pin">Pin Number:</label>
+      <input class="data" type="text" name="pin" id="pin" <?php if (isset($user['pin'])) {
+                                                            echo "value='$user[pin]'";
+                                                          } ?> required>
+      <label for="password">Password:</label>
+      <span class="password-requirements">Passwords must be at least 8 characters and
+        contain at least 1 number, 1 capital letter and 1 special character</span>
+      <input class="data" type="password" name="password" id="password" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+      <input type="submit" class="sign-button" name="submit" id="updateInfo" value="Update Profile">
+      <input type="hidden" name="action" value="register">
 
-    <section class="info-edit">
-      <h2>Change your data</h2>
-      <form action=""></form>
-    </section>
+    </form>
   </main>
 
 
