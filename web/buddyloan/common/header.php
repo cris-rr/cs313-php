2de64d7f0d981f648b3e6734a91cc1f15852932e
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-02-23 15:39:32 -0500

    CHECKING PATHS

--- web/buddyloan/common/header.php
+++ web/buddyloan/common/header.php
@@ -1,16 +1,22 @@
 <?php
 $folder = $_SERVER['DOCUMENT_ROOT'];
+if ($folder == '/app/web') {
+  $rootPath = "$folder/buddyloan";
+} else {
+  $rootPath = '..';
+}
+
 if ($_SESSION['loggedin']) {
   //echo "<span>Welcome $cookieFirstname</span>";
   $displayWelcome = "<p>Welcome $_SESSION[fullName]</p>
-                      <a class='signout' href='../accounts?action=logout'>Sign Out</a>
+                      <a class='signout' href='$rootPath/accounts?action=logout'>Sign Out</a>
                     ";
   $displayMenu = "
     <ul class='menubar'>
-    <li class='menu-item'><a class='pageHome' href='../'>Home</a></li>
-    <li class='menu-item'><a class='pageAdmin' href='../accounts/?action=admin' title='view buddies page'>Admin</a></li>
-    <li class='menu-item'><a class='pageBuddies' href='../buddies/' title='view Buddies page'>Buddies</a></li>
-    <li class='menu-item'><a class='pageTransactions' href='../transactions/' title='view Transactions page'>Transactions</a></li>
+    <li class='menu-item'><a class='pageHome' href='$rootPath/'>Home</a></li>
+    <li class='menu-item'><a class='pageAdmin' href='$rootPath/accounts/?action=admin' title='view buddies page'>Admin</a></li>
+    <li class='menu-item'><a class='pageBuddies' href='$rootPath/buddies/' title='view Buddies page'>Buddies</a></li>
+    <li class='menu-item'><a class='pageTransactions' href='$rootPath/transactions/' title='view Transactions page'>Transactions</a></li>
     </ul>";
 } else {
   $displayWelcome = '';
@@ -18,7 +24,7 @@ if ($_SESSION['loggedin']) {
 ?>
 <div class="header-top">
   <div class="logo">
-    <img src="../images/site/buddy256.png" alt="BuddyLoan">
+    <img <? echo "src=$rootPath/images/site/buddy256.png" ?> alt="BuddyLoan">
   </div>
   <div class="app-title">
     <h1><span class="title-first">B</span>uddy <span class="title-first">L</span>oan</h1>
@@ -35,9 +41,7 @@ if ($_SESSION['loggedin']) {
 
 <nav>
   <?php
-  echo $folder;
   if (isset($displayMenu)) {
-
     echo $displayMenu;
   }
   ?>
