e03a5ee9d02ac6f0b4c699a27f5929b3f34bd858
cuervo <59718617+cris-rr@users.noreply.github.com>
2021-02-23 15:54:41 -0500

    CHECKING PATHS 4

--- web/buddyloan/view/home.php
+++ web/buddyloan/view/home.php
@@ -1,3 +1,11 @@
+<?php
+$folder = $_SERVER['DOCUMENT_ROOT'];
+if ($folder == '/app/web') {
+  $rootPath = "/buddyloan";
+} else {
+  $rootPath = '..';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,8 +13,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="../images/site/buddy32.png" type="image/x-icon">
-  <link rel="stylesheet" href="../css/main.css">
+  <link rel="shortcut icon" <? echo "href='$rootPath/images/site/buddy32.png'" ?> type="image/x-icon">
+  <link rel="stylesheet" <? echo "href='$rootPath/css/main.css'" ?>>
   <title>BuddyLoan</title>
 </head>
 
